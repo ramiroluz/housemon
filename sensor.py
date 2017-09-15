7d913b86e82c54ba729af1d1705b9188847d0e1d
Ramiro Batista da Luz <ramiroluz@gmail.com>
2017-09-15 17:01:31 -0300

    Implements sensor parser.

--- sensor.py
+++ sensor.py
@@ -3,23 +3,22 @@ import re
 class Energy:
     '''
     expressions = ['(Device:.*;) Alarms', '^.*(Alarms:.*;) Power', '^.*(Line:.*;) Peaks', '^.*(Peaks:.*;) FFT Re', '^.*(FFT Re:.*;) FFT Img', '^.*(FFT Img:.*;) UTC Time', '^.*(UTC Time:.*;) hz', '^.*(hz:.*;) Wifi Strength', '^.*(WiFi Strength:.*;)']
-int_regex = re.compile('^[-]?[1-9]\d*$')
-float_regex = re.compile('^-?\d+\.\d+$')
-def extract_group_values(group):
-    group_name, group_values = group[0].split(':')
-    group_values = [value.replace(';', '') for value in group_values.split()]
-    group_values = [value.split('=') for value in group_values]
-    sizes = map(len, group_values)
-    pairs = all([size==2 for size in sizes])
-    if pairs:
-        return {group_name: dict(group_values)}
-    else:
-        return {group_name: [v[0] for v in group_values]}
     '''
     def __init__(self):
         self.int_regex = re.compile('^[-]?[1-9]\d*$')
         self.float_regex = re.compile('^-?\d+\.\d+$')
         self.datetime_regex = re.compile('^ ?\d{4}-\d{2}-\d{1,2} \d{2}:\d{2}:\d{2};$')
+        self.device_regex = '(Device:.*;) Alarms'
+        self.alarms_regex = '^.*(Alarms:.*;) Power'
+        self.power_regex = '^.*(Power:.*;) Line'
+        self.line_regex = '^.*(Line:.*;) Peaks'
+        self.peaks_regex = '^.*(Peaks:.*;) FFT Re'
+        self.fft_re_regex = '^.*(FFT Re:.*;) FFT Img'
+        self.fft_img_regex = '^.*(FFT Img:.*;) UTC Time'
+        self.utc_time_regex = '^.*(UTC Time:.*;) hz'
+        self.hz_regex = '^.*(hz:.*;) WiFi Strength'
+        self.wifi_strength_regex = '^.*(WiFi Strength:.*;) Dummy'
+        self.dummy_regex = '^.*(Dummy:.*)'
 
     def check_value(self, expr, value):
         regex = re.compile(expr)
@@ -57,39 +56,54 @@ def extract_group_values(group):
         return match.groups()
 
     def parse(self, data):
-        return {
-            'Device': {
-                'ID': 10,
-                'Fw': 16071801,
-                'Evt': 2
-            },
-            'Alarms': {
-                'CoilRevesed': 'OFF'
-            },
-            'Power': {
-                'Active': '289W',
-                'Reactive': '279var',
-                'Appearent': '403VA'
-            },
-            'Line': {
-                'Current': '1.75A',
-                'Voltage': '230.08V',
-                'Phase': '-43,841rad'
-            },
-            'Peaks': [
-                1041.000, 1051.000, 1058.000, 1051.000, 1049.000, 1047.000,
-                1054.000, 1059.000, 1057.000, 1060.000
-            ],
-            'FFT Re': [
-                -257863.00, 102815.00, -64043.00, 48516.00, 59599.00, -4223.00,
-                -43441.00, 23559.00, -24518.00
-            ],
-            'FFT Img': [
-                481910.00, -14891.00, 69871.00, -7130.00, 43860.00, 34204.00,
-                55951.00, -6945.00, 26131.00
-            ],
-            'UTC Time': '2016-10-4 16:47:50',
-            'hz': 49.87,
-            'WiFi Strength': -62,
-            'Dummy': 20
-        }
+        result = {}
+
+        group_txt = self.parse_group(self.device_regex, data)
+        group = self.parse_values(group_txt)
+        result.update(group)
+
+        group_txt = self.parse_group(self.alarms_regex, data)
+        group = self.parse_values(group_txt)
+        result.update(group)
+
+        group_txt = self.parse_group(self.power_regex, data)
+        group = self.parse_values(group_txt)
+        result.update(group)
+
+        group_txt = self.parse_group(self.line_regex, data)
+        group = self.parse_values(group_txt)
+        result.update(group)
+
+        group_txt = self.parse_group(self.peaks_regex, data)
+        group = self.parse_values(group_txt)
+        result.update(group)
+
+        group_txt = self.parse_group(self.peaks_regex, data)
+        group = self.parse_values(group_txt)
+        result.update(group)
+
+        group_txt = self.parse_group(self.fft_re_regex, data)
+        group = self.parse_values(group_txt)
+        result.update(group)
+
+        group_txt = self.parse_group(self.fft_img_regex, data)
+        group = self.parse_values(group_txt)
+        result.update(group)
+
+        group_txt = self.parse_group(self.utc_time_regex, data)
+        group = self.parse_values(group_txt)
+        result.update(group)
+
+        group_txt = self.parse_group(self.hz_regex, data)
+        group = self.parse_values(group_txt)
+        result.update(group)
+
+        group_txt = self.parse_group(self.wifi_strength_regex, data)
+        group = self.parse_values(group_txt)
+        result.update(group)
+
+        group_txt = self.parse_group(self.dummy_regex, data)
+        group = self.parse_values(group_txt)
+        result.update(group)
+
+        return result
